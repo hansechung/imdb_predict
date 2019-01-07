cb2dddd86a4917afc226ccab6ef16903ef4b6b74
Hanse Chung <42901056+hansechung@users.noreply.github.com>
2019-01-07 13:31:09 -0500

    First draft of IMDB - basic 2 layer NN
    Predict positive / negative reviews from word count

---
+++ imdb_x201.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  7 11:33:18 2019
+
+@author: Hanse
+"""
+
+import tensorflow as tf
+import keras
+import numpy as np
+
+from keras.datasets import imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros(len(sequences), dimension)
+    for i, sequence in enumerate(sequences):
+        results[i, sequence]=1
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000, )))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+from keras import optimizers
+from keras import losses
+from keras import metrics
+
+# Choose one of the three below
+
+model.compile(optimizer='rmsprop', 
+              loss='binary_crossentropy', 
+              metrics=['accuracy'])
+
+# Custom optimizer
+#model.compile(optimizer=optimizers.RMSprop(lr=0.001), 
+#              loss='binary_crossentropy', 
+#              metrics=['accuracy'])
+
+# Custom losses
+#model.compile(optimizer=optimizers.RMSprop(lr=0.001), 
+#              loss=losses.binary_crossentropy, 
+#              metrics=[metrics.binary.accuracy])
+
+# Training data
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train, partial_y_train,
+                    epochs=20, batch_size=512, 
+                    validation_data=(x_val, y_val))
+
+
+# Plot history of fitting results
+import matplotlib.pyplot as plt
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# Plot training and validation loss
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf()
+
+# Plot training and validation accuracy
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+results = model.evaluate(x_test, y_test)
+
+
